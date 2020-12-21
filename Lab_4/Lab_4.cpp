e9299d40eecc0d4d0a9e3c906e0e219fccf43e4c
Dacat01 <danko.gamal@ukr.net>
2020-12-21 16:46:34 +0200

    Lab_4

--- Lab_4/Lab_4.cpp
+++ Lab_4/Lab_4.cpp
@@ -9,34 +9,34 @@ using namespace std;
 class Fantazer
 {
 public:
-    std::vector< string > binary_exponent_values;
+    std::vector < string > binary_exponent_values;
 
     string sequence;
     int number;
-    int result = 0;
+    int result = 0;            
 
 
     void check_string(void)
     {
-        int id = binary_exponent_values.size() - 1;                                    // Set to highest value in vector 
-   
-        while (sequence.length() != 0)       
+        int id = binary_exponent_values.size() - 1;                                    // Set to highest index in vector 
+
+        while (sequence.length() != 0)
         {
             if (sequence.compare(0, binary_exponent_values[id].length(), binary_exponent_values[id]) == 0) // If first X characters is equal
             {
-                result++;                                         
+                result++;                                                               
 
                 sequence.erase(0, binary_exponent_values[id].length());                // delete first X characters 
 
-                id = binary_exponent_values.size() - 1;                                // Set to highest value in vector 
+                id = binary_exponent_values.size() - 1;                                // Set to highest index in vector 
             }
             else
             {
-                id--;           
+                id--;
 
-                if (id == -1)                                                       // If string can't be cutted in pieces
+                if (id == -1)                                                            
                 {
-                    result = -1;
+                    result = -1;                                                        // If string can't be cutted in pieces
                     return;
                 }
             }
@@ -44,21 +44,18 @@ public:
     }
 
 
-    void count_power( int length)
+    void count_power(int length)
     {
-        long long value=0;
+        long long value = 0;
         int counter = 0;
-       
+
         string binary_string;
         bool bin;
 
-        while (value < UINT_MAX)
+        while (value < UINT_MAX)                                        // value is limited to UINT_MAX (+4,294,967,295)
         {
             value = pow(number, counter);
             counter++;
-
-//            if (tmp > UINT_MAX)     
-//               return;
 
             if (value < UINT_MAX)
             {
@@ -70,8 +67,11 @@ public:
                 }
                 reverse(binary_string.begin(), binary_string.end());    // Decimal to Binary conversion end
 
-                if (sequence.find(binary_string) != std::string::npos)  // If bin_string is in given string
-                    binary_exponent_values.push_back(binary_string);        
+                if (sequence.length() < binary_string.length())
+                    return;
+
+                if (sequence.find(binary_string) != std::string::npos)  // If bin_string is in sequence
+                    binary_exponent_values.push_back(binary_string);    
 
                 binary_string = "";
             }
@@ -93,7 +93,7 @@ public:
 
             if ((position = line.find(delimiter)) != std::string::npos)
             {
-                sequence = line.substr(0, position);
+                sequence = line.substr(0, position);                    
                 line.erase(0, position + delimiter.length());
                 number = stoi(line);
             }
@@ -109,7 +109,7 @@ public:
         count_power(sequence.length());
         check_string();
 
-        std::cout << "result = " << result << endl << endl;
+        std::cout << "result = " << result << endl;
     }
 
 };
@@ -120,8 +120,8 @@ int main()
 {
     string path = "fantazer.txt";
 
-    Fantazer myObj;
-    
-    myObj.fantazer(path);
-   
+    Fantazer fantazerObj;
+
+    fantazerObj.fantazer(path);
+
 }
---
+++ Lab_4/UnitTest.cpp
@@ -0,0 +1,103 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../Lab_4/Lab_4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(Fantazer_1)
+		{
+			Fantazer fantazerObj;
+
+			fantazerObj.sequence = "101110011";
+			//						101 11001 1
+			fantazerObj.number = 5;
+
+			fantazerObj.count_power(fantazerObj.sequence.length());
+			fantazerObj.check_string();
+			
+			Assert::AreEqual(fantazerObj.result,3);
+		}
+
+
+		TEST_METHOD(Fantazer_2)
+		{
+			Fantazer fantazerObj;
+
+			fantazerObj.sequence = "101101101";
+			//						101 101 101
+			fantazerObj.number = 5;
+
+			fantazerObj.count_power(fantazerObj.sequence.length());
+			fantazerObj.check_string();
+
+			Assert::AreEqual(fantazerObj.result, 3);
+		}
+
+
+		TEST_METHOD(Fantazer_3)
+		{
+			Fantazer fantazerObj;
+
+			fantazerObj.sequence = "11010001000000001001000011001";
+			//						1  101000100000000  10010000  1100	1				
+			fantazerObj.number = 12;
+			
+			fantazerObj.count_power(fantazerObj.sequence.length());
+			fantazerObj.check_string();
+
+			Assert::AreEqual(fantazerObj.result, 5);
+		}
+
+		TEST_METHOD(Fantazer_4)
+		{
+			Fantazer fantazerObj;
+
+			fantazerObj.sequence = "110011111010000100110111110101111000011";
+			//						11001 11110100001001 1011111010111100001 1
+			fantazerObj.number = 25;
+
+			fantazerObj.count_power(fantazerObj.sequence.length());
+			fantazerObj.check_string();
+
+			Assert::AreEqual(fantazerObj.result, 4);
+		}
+
+		TEST_METHOD(Fantazer_5)
+		{
+			Fantazer fantazerObj;
+
+			fantazerObj.sequence = "10011100110100001101110001011110010001010010011000000110111111000110100100001101011101000001";
+			//						1001 1100110100001 10111000101111001000101001001 10000001101111110001 10100100001101011101000001	
+			fantazerObj.number = 9;
+
+			fantazerObj.count_power(fantazerObj.sequence.length());
+			fantazerObj.check_string();
+
+			Assert::AreEqual(fantazerObj.result, 5);
+		}
+	
+		TEST_METHOD(Fantazer_6)
+		{
+			Fantazer fantazerObj;
+
+			fantazerObj.sequence = "010000001";
+			//						0 1000000 1
+			fantazerObj.number = 4;
+
+			fantazerObj.count_power(fantazerObj.sequence.length());
+			fantazerObj.check_string();
+
+			Assert::AreEqual(fantazerObj.result, -1);
+		}
+
+	};
+
+};
+
