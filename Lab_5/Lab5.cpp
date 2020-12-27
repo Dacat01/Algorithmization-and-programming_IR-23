1fb6ba6a83883f57227bbb68259656f9a6100312
Dacat01 <danko.gamal@ukr.net>
2020-12-27 19:12:53 +0200

    added Lab_5

---
+++ Lab_5/Lab5.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> KMP(string line, string pattern)
+{
+    vector<int> table(pattern.size() + 1, -1);
+    vector<int> matches;
+    int line_position = 0;          //the position of the current character in line
+    int pattern_position = 0;       //the position of the current character in pattern
+    int pos;
+
+    if (pattern.size() == 0)
+    {
+        matches.push_back(-1);
+        return matches;
+    }
+
+
+    for (int i = 1; i <= pattern.size(); i++)
+    {
+        pos = table[i - 1];
+
+        while (pos != -1 && pattern[pos] != pattern[i - 1])
+            pos = table[pos];
+
+        table[i] = pos + 1;     
+
+ //       cout << "T[i]: " << table[i] << endl;
+    }
+
+    while (line_position < line.size())
+    {
+        if (pattern[pattern_position] == line[line_position])
+        {
+            line_position++;
+            pattern_position++;
+      
+            if (pattern_position == pattern.size())
+            {
+                matches.push_back(line_position - pattern_position);
+                pattern_position = table[pattern_position];   
+            }
+        }
+        else 
+        {
+            pattern_position = table[pattern_position];
+            if (pattern_position < 0)
+            {
+                line_position++;
+                pattern_position++;
+            }
+        }
+
+    }
+
+    return matches;
+}
+
+int main()
+{
+    string str = "abcabcdAAAABAAAAabcabcdABBBAAAABabcabcd";
+    string patt = "abcabcd";
+
+    vector<int> results = KMP(str, patt);
+
+    for (int i = 0; i < results.size(); i++) {
+        cout << "Pattern found at location: " << results[i] << endl;
+    }
+}
---
+++ Lab_5/UnitTest1.cpp
@@ -0,0 +1,70 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../Lab5/Lab5.cpp"
+#include <vector>
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string str = "abcabcdAAAABAAAAABBBAAAABabcabcd";
+			string pattern = "abcabcd";
+
+			vector<int> results = KMP(str, pattern);
+
+			Assert::AreEqual(0, results[0]);
+			Assert::AreEqual(25, results[1]);
+		}
+
+		TEST_METHOD(TestMethod_2)
+		{
+			string str = "abcdefg";
+			string pattern = "";
+
+			vector<int> results = KMP(str, pattern);
+
+			Assert::AreEqual(-1, results[0]);
+
+		}
+
+		TEST_METHOD(TestMethod_3)
+		{
+			string str = "ABC ABCDAB ABCDABCDABDE";
+			string pattern = "ABCDABD";
+
+			vector<int> results = KMP(str, pattern);
+
+			Assert::AreEqual(15, results[0]);
+
+		}
+
+		TEST_METHOD(TestMethod_4)
+		{
+			string str = "THIS IS A TEST TEXT";
+			string pattern = "TEST";
+
+			vector<int> results = KMP(str, pattern);
+
+			Assert::AreEqual(10, results[0]);
+		}
+
+		TEST_METHOD(TestMethod_5)
+		{
+			string str = "123456711567";
+			string pattern = "567";
+
+			vector<int> results = KMP(str, pattern);
+
+			Assert::AreEqual(4, results[0]);
+			Assert::AreEqual(9, results[1]);
+		}
+
+
+	};
+}
